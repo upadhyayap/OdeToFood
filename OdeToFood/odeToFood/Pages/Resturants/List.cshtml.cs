191884d7f14d55304e53c814d7d41713a33de407
Anand Upadhyay <upadhyayap.smit@gmail.com>
2019-10-09 18:28:02 +0530

    Finished asp.net core mvc tutorial

--- OdeToFood/odeToFood/Pages/Resturants/List.cshtml.cs
+++ OdeToFood/odeToFood/Pages/Resturants/List.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using OdeToFood.Core;
 using OdeToFood.Data;
 
@@ -14,18 +15,22 @@ namespace odeToFood.Pages.Resturants
     {
         private readonly IConfiguration config;
         private readonly IResturantData resturantData;
+        private readonly ILogger<ListModel> logger;
+
         public IEnumerable<Resturant> Resturants { get; set; }
         [BindProperty(SupportsGet = true)]
         public string SearchTerm { get; set; }
-        public ListModel(IConfiguration config, IResturantData resturantData)
+        public ListModel(IConfiguration config, IResturantData resturantData, ILogger<ListModel> logger)
         {
             this.config = config;
             this.resturantData = resturantData;
+            this.logger = logger;
         }
         public string Message { get; set; }
 
         public void OnGet()
         {
+            logger.LogError("Executing Resturant list");
             Message = config["Message"];
             Resturants = resturantData.GetResturantsByName(SearchTerm);
         }
